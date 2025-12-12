1cfa07524d9e2dea2e71bc46bc6bed269bc3bcef
Owen <omccandlish@gamil.com>
2025-12-12 11:12:00 -0500

    Removed unecessary condition

--- src/mem/cache/base.cc
+++ src/mem/cache/base.cc
@@ -563,7 +563,7 @@ BaseCache::recvTimingResp(PacketPtr pkt)
         bool allocate = (writeAllocator && mshr->wasWholeLineWrite) ?
             writeAllocator->allocate() : mshr->allocOnFill();
         // if victim cache, force no allocation for demand misses
-        allocate = (isVictimCache && pkt->isRead()) ? false : allocate;
+        allocate = (isVictimCache) ? false : allocate;
 
         blk = handleFill(pkt, blk, writebacks, allocate);
         assert(blk != nullptr);
